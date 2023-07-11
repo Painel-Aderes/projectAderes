719e38ac3afd02416a5b28d8a5f88eed19d71135
endersonpb <65699677+EndersonpereiraBarcelos@users.noreply.github.com>
2023-07-11 13:15:31 -0300

    Update apis urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,12 +4,12 @@ from . import views
 urlpatterns = [
     #Artesões
     path('artesoes/', views.getArtesoes), #Puxa todos os dados
-    path ('artesoesUpdate/<str:pk>', views.GetPutDellArtesoes), #Puxa, Atualiza e Deleta dados.
-    path ('create', views.GetPostArtesoes),#Puxa todos os dados e Cria
+    path ('artesoes/update/<str:pk>', views.GetPutDellArtesoes), #Puxa, Atualiza e Deleta dados.
+    path ('artesoes/create', views.GetPostArtesoes),#Puxa todos os dados e Cria
     ##JUNTA COMERCIAL
     path ('junta/', views.getJuntacomercial),
-    path ('juntaCreate', views.getPostJuntacomercial),
-    path ('juntaUpdate/<str:pk>', views.GetPutDeleteJuntacomercial),
+    path ('junta/create', views.getPostJuntacomercial),
+    path ('junta/update/<str:pk>', views.GetPutDeleteJuntacomercial),
     ##MICROCREDITO
     path('microcredito/', views.getMicroCredito),
     path('microcredito/read', views.getPostMicroCredito),
